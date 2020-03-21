d7b987c134c4c964afed7b5580ccbb5ea6da2adb
Shun Miyaki <shunmiyaki@hotmail.com>
2020-03-21 01:16:16 -0700

    200321 / 200319_dynamicRender - Added a dynamic render to see if it can be implemented instead of router approach. - Procced to make Typography Section and TestAlpha and TestBeta.

---
+++ src/components/PrimarySection.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Alpha from "./TestAlpha";
+import Beta from "./TestBeta";
+
+class PrimarySection extends Component {
+  content = {
+    alpha: Alpha,
+    beta: Beta
+  };
+
+  render() {
+    const SelectedContent = this.content[this.props.content];
+    return <SelectedContent/>;
+  }
+}
+
+export default PrimarySection;
