5e120ea88242ab2848814e8598c5460d73246ea3
Shun Miyaki <shunmiyaki@hotmail.com>
2020-03-24 00:02:53 -0700

    200324 / 200322_prepareTypographyLocales - Locales added. - Proceed to performing locale-specific adjustments.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import Navigation from './components/Navigation';
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faBars, faExternalLinkAlt, faFileAlt } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faExternalLinkAlt, faFileAlt, faLanguage } from '@fortawesome/free-solid-svg-icons'
 import SectionPrimary from './components/SectionPrimary';
 
-library.add(faBars, faExternalLinkAlt, faFileAlt);
+library.add(faBars, faExternalLinkAlt, faFileAlt, faLanguage);
 
 function App() {
   const [content, setContent] = useState("typography");
---
+++ src/components/SectionTypographySpanish.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+class SectionTypographyEnglish extends Component {
+  render() {
+    return (
+      <>
+        <code className="localeCode"><FontAwesomeIcon icon="language" className="prefixSVG" />ES-ES</code>
+        <h1>
+          Encabezando uno
+          <span> Encabezado de página / Máx. 1 </span>
+        </h1>
+        <h2>
+          Encabezado dos
+          <span> Sección / Encabezado </span>
+        </h2>
+        <h3>
+          Encabezado tres
+          <span> Sección / Encabezado / Sub </span>
+        </h3>
+        <h4>
+          Título cuatro
+          <span> Elemento / Encabezado </span>
+        </h4>
+        <h5>
+          Título cinco
+          <span> Elemento / Encabezado / Sub </span>
+        </h5>
+        <h6>
+          Título seis
+          <span> Elemento / Encabezado / Terciario </span>
+        </h6>
+        <hr/>
+        <p> Un párrafo (del griego antiguo παράγραφος párrafoos, "escribir al lado" o "escrito al lado") es una unidad autónoma de un discurso escrito que trata un punto o idea en particular. Un párrafo consta de más de una oración. [1] [2] Aunque la sintaxis de cualquier idioma no lo requiere, los párrafos suelen ser una parte esperada de la escritura formal, y se utilizan para organizar una prosa más larga. </p>
+        <hr/>
+        <div className="typographyElementExample">
+          <h4> Elemento Ejemplo / h4 </h4>
+          <div>
+            <h5> Paso 1: Alfa / h5 </h5>
+            <p> Un párrafo (del griego antiguo παράγραφος párrafoos, "escribir al lado" o "escrito al lado") es una unidad autónoma de un discurso escrito que trata un punto o idea en particular. Un párrafo consta de más de una oración. [1] [2] Aunque la sintaxis de cualquier idioma no lo requiere, los párrafos suelen ser una parte esperada de la escritura formal, y se utilizan para organizar una prosa más larga. </p>
+          </div>
+          <div>
+            <h5> Paso 2: Beta / h5 </h5>
+            <p> Un párrafo (del griego antiguo παράγραφος párrafoos, "escribir al lado" o "escrito al lado") es una unidad autónoma de un discurso escrito que trata un punto o idea en particular. Un párrafo consta de más de una oración. [1] [2] Aunque la sintaxis de cualquier idioma no lo requiere, los párrafos suelen ser una parte esperada de la escritura formal, y se utilizan para organizar una prosa más larga. </p>
+            <p> Un párrafo (del griego antiguo παράγραφος párrafoos, "escribir al lado" o "escrito al lado") es una unidad autónoma de un discurso escrito que trata un punto o idea en particular. Un párrafo consta de más de una oración. [1] [2] Aunque la sintaxis de cualquier idioma no lo requiere, los párrafos suelen ser una parte esperada de la escritura formal, y se utilizan para organizar una prosa más larga. </p>
+          </div>
+          <div>
+            <h5> Paso 3: Charlie / h5 </h5>
+            <p> Un párrafo (del griego antiguo παράγραφος párrafoos, "escribir al lado" o "escrito al lado") es una unidad autónoma de un discurso escrito que trata un punto o idea en particular. Un párrafo consta de más de una oración. [1] [2] Aunque la sintaxis de cualquier idioma no lo requiere, los párrafos suelen ser una parte esperada de la escritura formal, y se utilizan para organizar una prosa más larga. </p>
+            <p> Un párrafo (del griego antiguo παράγραφος párrafoos, "escribir al lado" o "escrito al lado") es una unidad autónoma de un discurso escrito que trata un punto o idea en particular. Un párrafo consta de más de una oración. [1] [2] Aunque la sintaxis de cualquier idioma no lo requiere, los párrafos suelen ser una parte esperada de la escritura formal, y se utilizan para organizar una prosa más larga. </p>
+            <p> Un párrafo (del griego antiguo παράγραφος párrafoos, "escribir al lado" o "escrito al lado") es una unidad autónoma de un discurso escrito que trata un punto o idea en particular. Un párrafo consta de más de una oración. [1] [2] Aunque la sintaxis de cualquier idioma no lo requiere, los párrafos suelen ser una parte esperada de la escritura formal, y se utilizan para organizar una prosa más larga. </p>
+          </div>
+        </div>
+      </>
+    );
+  }
+
+}
+
+export default SectionTypographyEnglish;
