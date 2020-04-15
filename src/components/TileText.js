32657404d0ad6d15bf0baadb98b79b9a812b1d7a
Shun Miyaki <shunmiyaki@hotmail.com>
2020-04-15 00:20:31 -0700

    200415 / 200409_tileRefactor - Tile css refactor complete. - Proceed to Modal Component.

--- src/components/TileText.js
+++ src/components/TileText.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 
 class TileText extends Component {
-  classConfig = this.props.tileCard === 'true' ? 'tileTextContainer tileCard' : 'tileTextContainer tileFlat';
+  classConfig = this.props.tileCard === 'true' ? 'tileElement tileCard' : 'tileElement tileFlat';
   render() {
     return (
       <div className={this.classConfig}>
-        <h6 className="tileTextHeader">{this.props.header}</h6>
-        <p className="tileTextBody">{this.props.body}</p>
-        <div className="tileTextFooter">
-          <p className="tileDateStamp">Mar.31 2020</p>
-          <a className="tileViewMore" href="./">View More</a>
+        <div className="tileTextContainer">
+          <h6 className="tileTextHeader">{this.props.header}</h6>
+          <p className="tileTextBody">{this.props.body}</p>
+          <div className="tileFooter">
+            <p className="tileDateStamp">Mar.31 2020</p>
+            <a className="tileViewMore" href="./">View More</a>
+          </div>
         </div>
       </div>
     );
