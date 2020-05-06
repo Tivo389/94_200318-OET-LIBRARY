b5bcba153014d26801cfb6db1abbcb9ab460ccff
Shun Miyaki <shunmiyaki@hotmail.com>
2020-05-06 01:48:59 -0700

    200506 / 200430_carouselComponentBuild - End of day - Continue adding some script

--- src/components/SectionCarousel.js
+++ src/components/SectionCarousel.js
@@ -5,7 +5,7 @@ class SectionCarousel extends Component {
   render() {
     return (
       <>
-        <section className="sectionContainer">
+        <section className="sectionContainer hidden">
           <code><FontAwesomeIcon icon="file-alt"/> Carousel / 200425</code>
           <div className="linksContainer">
             <a
@@ -31,6 +31,22 @@ class SectionCarousel extends Component {
             </ul>
             <hr></hr>
             <ul>
+              <li>Carousel's are commonly used in the Landing Hero Section to cater for different promotions.</li>
+              <li>Blizzard had 6 carousels on auto scroll.</li>
+              <li>Blizzard also used a carousel to show its most recent dozen games.</li>
+              <li>Nintendo used 4 for the landing hero on auto scroll</li>
+              <li>Nintento used had 3 games per carousel slide, so 12 in total.</li>
+              <li>Logitech had 7 but the colour was not the best as the indicators were difficult to see.</li>
+              <li>Common aspects...</li>
+                <ul>
+                  <li>Minimal text, usualy just the product name and tagline of less than 35 characters.</li>
+                  <li>1-2 CTAs</li>
+                  <li>1-3 Images</li>
+                </ul>
+              <li>I got the impression that Carousel's are difficult to use as a generic component as their layout is heavily influenced by the content and space. It's not an effective component to display content and serves better to direct the user. Kind of like the content of a book or a menu.</li>
+            </ul>
+            <hr></hr>
+            <ul>
               <li>320 x 568 iPhone SE</li>
                 <ul>
                   <li>20px iOS Bar</li>
@@ -46,9 +62,6 @@ class SectionCarousel extends Component {
                 <li>1.41875 - 0.23125 = 1.1875</li>
                 <li>Rounding up is 1.20</li>
               </ul>
-            </ul>
-            <hr></hr>
-            <ul>
               <li>Considered 100vh but this doesn't account for OS bar and Browser Bars.</li>
               <li>In addition, the lack of a crop is more confusing.</li>
               <li>For a content-oriented scenario, the objective of mobile and desktop should be considered separately, due to the difference in available space.</li>
@@ -66,15 +79,55 @@ class SectionCarousel extends Component {
             </ul>
           </div>
         </section>
-        <section className="sectionContainer noPaddingBottom">
+        <section className="sectionContainer noPaddingBottom hidden">
           <code className="sectionHeader"><FontAwesomeIcon icon="horse-head"/> Carousel</code>
         </section>
         <div className="carouselElement">
           <div className="carouselContent">
-            <h6>Hello World</h6>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam temporibus ratione itaque iure sint? Incidunt earum venam.</p>
+            <div className="carouselContentItem">
+              <h6>Alpha</h6>
+            </div>
+            <div className="carouselContentItem">
+              <h6>Beta</h6>
+            </div>
+            <div className="carouselContentItem">
+              <h6>Charlie</h6>
+            </div>
+            <div className="carouselContentItem">
+              <h6>Delta</h6>
+            </div>
+            <div className="carouselContentItem">
+              <h6>Echo</h6>
+            </div>
+            <div className="carouselContentItem">
+              <h6>Foxtrot</h6>
+            </div>
+            <div className="carouselContentItem">
+              <h6>Golf</h6>
+            </div>
+            <div className="carouselContentItem">
+              <h6>Hotel</h6>
+            </div>
+            <div className="carouselContentItem">
+              <h6>India</h6>
+            </div>
+            <div className="carouselContentItem">
+              <h6>Juliett</h6>
+            </div>
+            <div className="carouselContentItem">
+              <h6>Kilo</h6>
+            </div>
+            <div className="carouselContentItem">
+              <h6>Lima</h6>
+            </div>
           </div>
           <div className="carouselNavigation">
+            <div className="carouselNavgationItem"></div>
+            <div className="carouselNavgationItem"></div>
+            <div className="carouselNavgationItem"></div>
+            <div className="carouselNavgationItem"></div>
+            <div className="carouselNavgationItem"></div>
+            <div className="carouselNavgationItem"></div>
             <div className="carouselNavgationItem"></div>
             <div className="carouselNavgationItem"></div>
             <div className="carouselNavgationItem"></div>
