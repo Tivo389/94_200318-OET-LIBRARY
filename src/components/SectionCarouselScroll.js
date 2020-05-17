f585537d5a995a7e80c424a65c91adb37e39ed5a
Shun Miyaki <shunmiyaki@hotmail.com>
2020-05-17 01:06:13 -0700

    200517 / 200516_carouselSwipeBuild - Carousel Swipe Complete - Proceed to explore if scrollLeft will prevent vertical-scroll swaying

--- src/components/SectionCarouselScroll.js
+++ src/components/SectionCarouselScroll.js
@@ -9,20 +9,28 @@ class SectionCarouselScroll extends Component {
         <section className="sectionContainer">
           <code><FontAwesomeIcon icon="file-alt"/> Carousel / Scroll / 200509</code>
           <ul>
-            <li>Note that this UI-approach will make it difficult for users with devices (i.e trackpad, mouse, etc) that do not have horizontal scroll features.</li>
+            <li>Note that these are more to focus on each UI and may not be complete on their own.</li>
+            <ul>
+              <li>
+                For example, a scroll-only UI makes it difficult for users with devices (i.e trackpad, mouse, etc) that do not have horizontal scroll features.
+              </li>
+              <li>On this occasion the scroll UI is only applied to the mobile breakpoint so horizontal scroll for desktop environment should not be an concern.</li>
+              <li>As a general rule of thumb a click/tap UI should be the basic.</li>
+            </ul>
             <li>Shift + Scroll / Seems to work on macOS but this may not be common knowledge.</li>
             <li>Clicking on the Carousel to make it the active/focused element and navigating the content with Left/Right-arrow is an option but will be an issue if the content itself is clickable.</li>
             <ul>
               <li>For mobile, not so much an issue. For desktop, limiting clickable area might be a potential solution.</li>
             </ul>
-            <li>In general not a good experience on mobile</li>
+            <li>Notes regarding UI</li>
             <ul>
+              <li>In general not a good experience on mobile</li>
               <li>Scroll-fatigue &amp; Micro-scrolling</li>
               <li>Finger blocks content</li>
               <li>Conflicts with browser's back-feature</li>
               <li>One-way content accessibility</li>
+              <li>Good vertical space-saving on mobile</li>
             </ul>
-            <li>Good vertical space-saving on mobile</li>
           </ul>
         </section>
         <section className="sectionContainer noPaddingBottom">
--- src/components/SectionCarouselSwipe.js
+++ src/components/SectionCarouselSwipe.js
@@ -9,7 +9,29 @@ class SectionCarouselSwipe extends Component {
         <section className="sectionContainer">
           <code><FontAwesomeIcon icon="file-alt"/> Carousel / Swipe / 200516</code>
           <ul>
-            <li>---</li>
+            <li>Note that these are more to focus on each UI and may not be complete on their own.</li>
+            <ul>
+              <li>
+                For example, a UI that involves swiping won't be effective on a desktop device.'
+              </li>
+              <li>As a general rule of thumb a click/tap UI should be the basic.</li>
+            </ul>
+            <li>Notes regarding UI</li>
+            <ul>
+              <li>Due to less scrolling and no micro-scrolling, a better experience.</li>
+              <li>Finger blocking content issue is reduced.</li>
+              <li>Can still conflict with browser's back-feature</li>
+              <li>One-way content accessibility</li>
+              <li>Good vertical space-saving on mobile</li>
+              <li>Seems the horizontal-scrolling prevented veritcal-scroll sway.</li>
+              <ul>
+                <li>Since scrolling isn't used with this UI there is some vertical-scroll swaying.</li>
+                <li>Wait a min, would using .scrollLeft instead of translateX prevent vertical-scroll swaying?</li>
+                <ul>
+                  <li></li>
+                </ul>
+              </ul>
+            </ul>
           </ul>
         </section>
         <section className="sectionContainer noPaddingBottom">
