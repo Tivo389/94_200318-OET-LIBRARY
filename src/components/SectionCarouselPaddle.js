fd784b906328ca7f7ae3fd59d4da7c7f217bc18e
Shun Miyaki <shunmiyaki@hotmail.com>
2020-05-26 00:32:11 -0700

    200526 / 200521_carouselPaddleBuild - Made Paddle Conventional - Proceed with note-taking and then move to Bottom-UI

--- src/components/SectionCarouselPaddle.js
+++ src/components/SectionCarouselPaddle.js
@@ -7,13 +7,18 @@ class SectionCarouselPaddle extends Component {
     return (
       <>
         <section className="sectionContainer">
-          <code><FontAwesomeIcon icon="file-alt"/> Carousel / Paddle / 200517</code>
+          <code><FontAwesomeIcon icon="file-alt"/> Carousel / Paddle / Conventional / 200517</code>
           <ul>
-            <li><span role='img' aria-label='thinking'>🤔</span></li>
+            <li>Considered an "Invisible Paddle UI" but decided not to make it since the general experience would be the same as the Bottom-UI <span role='img' aria-label='thinking'>🤔</span></li>
+            <ul>
+              <li>It would also require the current carousel item to be narrower, to display previous/next items.</li>
+              <li>In addition, when previous/next items are visible the user will probably more likely to swipe. An "Invisible Paddle" may reduce visual elements, but also lacks 'noticability' since it's not displayed.</li>
+            </ul>
+            <li>999 Continue jotting observations</li>
           </ul>
         </section>
         <section className="sectionContainer noPaddingBottom">
-          <code className="sectionHeader"><FontAwesomeIcon icon="horse-head"/> Carousel / Paddle</code>
+          <code className="sectionHeader"><FontAwesomeIcon icon="horse-head"/> Carousel / Paddle / Conventional</code>
         </section>
         <CarouselPaddle/>
       </>
