225c98db701c2837c21dd1e5b9c66cdd08af99e0
Shun Miyaki <shunmiyaki@hotmail.com>
2020-08-05 23:27:53 -0700

    200805 / 200805_table - Made more adjustments to the table - Added slight UI & UX enhancement - Added comparison of Default and Enhanced

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import Navigation from './components/Navigation';
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faBars, faExternalLinkAlt, faFileAlt, faLanguage, faNewspaper, faVideo, faCalendarAlt, faPlayCircle, faComments, faMapSigns, faWindowRestore, faTimes, faStar, faHorseHead } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faExternalLinkAlt, faFileAlt, faLanguage, faNewspaper, faVideo, faCalendarAlt, faPlayCircle, faComments, faMapSigns, faWindowRestore, faTimes, faStar, faHorseHead, faTable } from '@fortawesome/free-solid-svg-icons'
 import SectionPrimary from './components/SectionPrimary';
 
-library.add(faBars, faExternalLinkAlt, faFileAlt, faLanguage, faNewspaper, faVideo, faCalendarAlt, faPlayCircle, faComments, faMapSigns, faWindowRestore, faTimes, faStar, faHorseHead);
+library.add(faBars, faExternalLinkAlt, faFileAlt, faLanguage, faNewspaper, faVideo, faCalendarAlt, faPlayCircle, faComments, faMapSigns, faWindowRestore, faTimes, faStar, faHorseHead, faTable);
 
 function App() {
   const [content, setContent] = useState("table");
